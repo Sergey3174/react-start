2badc320fdec218be1f35c8633e5913ad26e40ef
Sergey <skauzerlfc@gmail.com>
2024-05-26 02:46:03 +0300

    change App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,29 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement('header', { className: 'App-header' }, [
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement('p', {}, [
+				'Edit ',
+				createElement('code', {}, 'src/App.js'),
+				' and save to reload.',
+			]),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', {}, new Date().getFullYear()),
+		]),
 	);
 };
-// Если правильно понял, то все в декларативном стиле. Потому что тут HTML разметка,
-// а один из лучших, наверное, примеров декларативного подхода в написании кода —
-// это язык разметки HTML.
